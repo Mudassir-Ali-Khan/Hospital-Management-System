c0b3b1528d92e3e8be58451c1adac7f509cbec9c
Riyan Irfan <rirfan@axisdatalytics.com>
2024-07-11 04:13:08 +0500

    Added light mode config, and some new styling

---
+++ assets/js/data-table.js
@@ -0,0 +1,4 @@
+document.getElementById('filterByDropdown').addEventListener('click', () => {
+    const customDropdown = document.getElementById('custom-dropdown');
+    customDropdown.classList.toggle('showDropdown');
+});
