4a81a7f9bb5c897a9522439fb5756f6384d7ba26
Mudassir-Ali-Khan <mudassir778.3@gmail.com>
2024-07-11 07:11:55 +0500

    Add full screen functionality to table

--- assets/js/data-table.js
+++ assets/js/data-table.js
@@ -2,3 +2,14 @@ document.getElementById('filterByDropdown').addEventListener('click', () => {
     const customDropdown = document.getElementById('custom-dropdown');
     customDropdown.classList.toggle('showDropdown');
 });
+
+const toggleFullscreen = () => {
+    const table = document.getElementById('dataTable');
+    if (!document.fullscreenElement) {
+        table.requestFullscreen().catch(err => {
+            alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+}
